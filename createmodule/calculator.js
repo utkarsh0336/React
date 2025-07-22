7f2f50d92c2b617cb6d4e2438050528803d983a3
Utkarsh Sahay <utkarshsahay@Utkarshs-MacBook-Air.local>
2025-07-22 21:41:57 +0530

    your message

---
+++ createmodule/calculator.js
@@ -0,0 +1,11 @@
+export let sumData = (a , b) => {
+    return a + b;
+}
+
+export let minData = (a , b) => {
+    return a - b;
+}
+
+export let c = "UKG";
+
+// export {sumData , minData};
---
+++ createmodule/index.js
@@ -0,0 +1,5 @@
+import {sumData as sum , minData, c} from "./calculator.js";
+
+console.log(sum(50,20));
+console.log(minData(50,20));
+console.log(c)
